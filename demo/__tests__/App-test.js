1165231290ff0e69f9b391e16bd5124f1f31f13c
aliasgharkarani <aliasgharkarani125@hotmail.com>
2020-03-11 19:54:11 +0500

    Test Cases for RNColorPlalette-test color-picker-test color-wheel git commit -m

--- demo/__tests__/App-test.js
+++ demo/__tests__/App-test.js
@@ -4,11 +4,10 @@
 
 import 'react-native';
 import React from 'react';
-import App from '../../App';
-
-// Note: test renderer must be required after react-native.
+import App from '../App';
 import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+test('renders correctly', () => {
+  const tree = renderer.create(<App />).toJSON();
+  expect(tree).toMatchSnapshot();
 });
